76dad649197e20874ddcfeed5a4e2078a38e2cc0
Garron Anderson <garronanderson4321@gmail.com>
2026-07-02 20:58:52 -0500

    add popover
    Add popover view with progress bar, reminder names, and reset buttons.

--- octoprint_maintenance_reminder/static/js/maintenance_reminder.js
+++ octoprint_maintenance_reminder/static/js/maintenance_reminder.js
@@ -6,7 +6,7 @@
  */
  $(function () {
 
-    function MaintenanceReminderSettingsViewModel(parameters) {
+    function MaintenanceReminderViewModel(parameters) {
         var self = this;
         window.maintenanceReminderVM = self;
 
@@ -47,6 +47,21 @@
             item.count(0);
         };
 
+        self.resetReminderByIndex = function (index) {
+          var reminder = self.pluginSettings.reminders()[index];
+
+          reminder.count(0);
+          self.settingsViewModel.saveData()
+
+
+          $(".maintenance-popover .bar")
+              .eq(index)
+              .css("width", "0%");
+          $(".maintenance-popover #count")
+              .eq(index)
+              .text("0");
+        };
+
         self.incrementReminder = function (item) {
           item.count(item.count() + 1);
         }
@@ -55,6 +70,67 @@
             return self.pluginSettings &&
                    self.pluginSettings.reminders().some(self.isAlert);
         });
+
+        $("#navbar_maintenance_reminder").popover({
+            trigger: "click",
+            placement: "bottom",
+            container: "body",
+            html: true,
+
+            template:
+                '<div class="popover maintenance-popover">' +
+                    '<div class="arrow"></div>' +
+                    '<h3 class="popover-title"></h3>' +
+                    '<div class="popover-content"></div>' +
+                '</div>',
+
+            title: "Maintenance Reminder",
+            content: function () {
+                return self.buildPopover();
+            }
+        });
+
+        self.buildPopover = function () {
+          var html = `<table class="table table-striped">
+			<tbody data-bind="foreach: pluginSettings.reminders">`
+      self.pluginSettings.reminders().forEach(function(r, i) {
+        var percent = Math.round(Math.min((r.count() / r.interval()) * 100, 100));
+
+        html += `<tr>
+            <td>${r.message()}</td>
+            <td style="text-align:right"><span id='count'>${r.count()}</span> / ${r.interval()} prints
+            <button class="btn btn-mini btn-primary reminder-reset" data-index='${i}'>
+              <i class="fa fa-refresh"></i> Reset</td>
+            </button>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div class="progress">`;
+
+        if (percent <= 50) {
+            html += `<div class="bar bar-info" style="width:${percent}%">${percent}%</div>`;
+        } else if (percent < 80) {
+            html += `<div class="bar bar-warning" style="width:${percent}%">${percent}%</div>`;
+        } else {
+            html += `<div class="bar bar-danger" style="width:${percent}%">${percent}%</div>`;
+        }
+
+        html += `
+                </div>
+            </td>
+        </tr>
+        `;
+      });
+
+      $(document).on("click", ".reminder-reset", function (e) {
+          e.preventDefault();
+
+          var index = $(this).data("index");
+          window.maintenanceReminderVM.resetReminderByIndex(index);
+      });
+
+      return html;
+    };
 
         self.onDataUpdaterPluginMessage = function(plugin, data) {
           if (plugin !== "maintenance_reminder") {
@@ -74,7 +150,7 @@
     }
 
     OCTOPRINT_VIEWMODELS.push([
-        MaintenanceReminderSettingsViewModel,
+        MaintenanceReminderViewModel,
         ["settingsViewModel"],
         [
             "#settings_plugin_maintenance_reminder",
